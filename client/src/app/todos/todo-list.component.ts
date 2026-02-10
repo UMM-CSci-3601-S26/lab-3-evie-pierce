a77c92962395e978a3f2d467882ad9b2c6497cba
Mindthemoods <prcrichards@gmail.com>
2026-02-10 14:04:47 -0600

    Fixed unused import

--- client/src/app/todos/todo-list.component.ts
+++ client/src/app/todos/todo-list.component.ts
@@ -14,7 +14,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink } from '@angular/router';
 import { catchError, combineLatest, of, switchMap, tap } from 'rxjs';
 import { Todo } from './todo';
-import { TodoCardComponent } from './todo-card.component';
+//import { TodoCardComponent } from './todo-card.component';
 import { TodoService } from './todo.service';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
