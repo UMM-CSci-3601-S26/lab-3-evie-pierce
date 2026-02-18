9d265f203d2174ab7ae4adc453748dc37c4574aa
Mindthemoods <prcrichards@gmail.com>
2026-02-18 12:11:55 -0600

    Add e2e test for add-todo.cy.ts
    Co-authored-by: Evelyn Williams <will7392@users.noreply.github.com>

---
+++ client/cypress/support/add-todo.po.ts
@@ -0,0 +1,59 @@
+import { Todo } from 'src/app/todos/todo';
+
+export class AddTodoPage {
+
+  private readonly url = '/todos/new';
+  private readonly title = '.add-todo-title';
+  private readonly button = '[data-test=confirmAddTodoButton]';
+  private readonly snackBar = '.mat-mdc-simple-snack-bar';
+  // private readonly nameFieldName = 'name';
+  // private readonly ageFieldName = 'age';
+  // private readonly companyFieldName = 'company';
+  // private readonly emailFieldName = 'email';
+  private readonly ownerFieldName = 'owner';
+  private readonly categoryFieldName = 'category';
+  private readonly bodyFieldName = 'body';
+  private readonly statusFieldName = 'status';
+  private readonly formFieldSelector = 'mat-form-field';
+  private readonly dropDownSelector = 'mat-option';
+
+  navigateTo() {
+    return cy.visit(this.url);
+  }
+
+  getTitle() {
+    return cy.get(this.title);
+  }
+
+  addTodoButton() {
+    return cy.get(this.button);
+  }
+
+  selectMatSelectValue(select: Cypress.Chainable, value: string) {
+    // Find and click the drop down
+    return select.click()
+      // Select and click the desired value from the resulting menu
+      .get(`${this.dropDownSelector}[value="${value}"]`).click();
+  }
+
+  getFormField(fieldName: string) {
+    return cy.get(`${this.formFieldSelector} [formcontrolname=${fieldName}]`);
+  }
+
+  getSnackBar() {
+    // Since snackBars are often shown in response to errors,
+    // we'll add a timeout of 10 seconds to help increase the likelihood that
+    // the snackbar becomes visible before we might fail because it
+    // hasn't (yet) appeared.
+    return cy.get(this.snackBar, { timeout: 10000 });
+  }
+
+  addTodo(newTodo: Todo) {
+    this.getFormField(this.ownerFieldName).type(newTodo.owner);
+    this.getFormField(this.categoryFieldName).type(newTodo.category.toString());
+    this.getFormField(this.bodyFieldName).type(newTodo.body);
+    // this.getFormField(this.statusFieldName).type(newTodo.status);
+    this.selectMatSelectValue(this.getFormField(this.statusFieldName), newTodo.status);
+    return this.addTodoButton().click();
+  }
+}
